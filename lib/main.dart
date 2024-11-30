ba7904e4564d09d0ea1b525422d23949cf5c2fc4
aden septian <adenseptian52@gmail.com>
2024-11-30 07:25:23 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://rfnnqfxfmirgzttwqpqp.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmbm5xZnhmbWlyZ3p0dHdxcHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MjY3MDgsImV4cCI6MjA0NzMwMjcwOH0.lrB1SzEPTNHfOsIClwl0eLK3XD2vdYvy23y4D-XrtXk',
+    url: 'https://ruusajasbqtrleebsqik.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dXNhamFzYnF0cmxlZWJzcWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MjYwNzYsImV4cCI6MjA0NzMwMjA3Nn0.i92C8LkUvJEsf7o927CTYtLoa4Pt_emIPCXbaszC-fk',
   );
   runApp(MyApp());
 }
